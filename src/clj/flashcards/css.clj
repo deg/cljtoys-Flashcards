a72e5f81e0eb4b85d64117fc9f958e2cb2300465
David Goldfarb <deg@degel.com>
2016-08-04 14:38:36 +0300

    [For BugzId:445] Backport static CSS to Garden

--- src/clj/flashcards/css.clj
+++ src/clj/flashcards/css.clj
@@ -2,14 +2,22 @@
   (:require [garden.def :refer [defstyles]]))
 
 (defstyles screen
-  [:.fc-card {:padding "1em",
-              :margin "3px",
-              :border-radius "12px",
-              :display "inline-block",
-              :min-width "6em",
-              :font-size "14pt",
-              :background-color "#F0FFFF",
-              :box-shadow "1px 2px 1px 0px rgba(0,0,0,0.75)",
-              }]
-  [:body {:color "red"}]
-  [:.level1 {:color "green"}])
+  [:.fc-card
+   {:padding "1em",
+    :margin "3px",
+    :border-radius "12px",
+    :display "inline-block",
+    :min-width "6em",
+    :font-size "14pt",
+    :background-color "#F0FFFF",
+    :box-shadow "1px 2px 1px 0px rgba(0,0,0,0.75)"}]
+  [:.fc-card:hover
+   {:background-color "#80FFFF"}]
+  [:.fc-card:active
+   {:background-color "#80FFFF"
+    :box-shadow "inset 1px 2px 1px 0px rgba(0,0,0,0.75)"}]
+  [:.fc-card:focus
+   {:outline "none"}]
+  [:.subject-word
+   {:font-size "28pt"}]
+)
