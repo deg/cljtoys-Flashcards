8f84a051e0fca139916bf1e336d5640ee1d90623
David Goldfarb <deg@degel.com>
2016-08-04 14:59:44 +0300

    [For BugzId:441] Do a better job on button states and focus

--- src/clj/flashcards/css.clj
+++ src/clj/flashcards/css.clj
@@ -1,23 +1,32 @@
 (ns flashcards.css
   (:require [garden.def :refer [defstyles]]))
 
-(defstyles screen
-  [:.fc-card
-   {:padding "1em",
-    :margin "3px",
-    :border-radius "12px",
-    :display "inline-block",
-    :min-width "6em",
-    :font-size "14pt",
-    :background-color "#F0FFFF",
-    :box-shadow "1px 2px 1px 0px rgba(0,0,0,0.75)"}]
-  [:.fc-card:hover
-   {:background-color "#80FFFF"}]
-  [:.fc-card:active
-   {:background-color "#80FFFF"
-    :box-shadow "inset 1px 2px 1px 0px rgba(0,0,0,0.75)"}]
-  [:.fc-card:focus
-   {:outline "none"}]
-  [:.subject-word
-   {:font-size "28pt"}]
-)
+(let [box-shadow "1px 2px 1px 0px rgba(0,0,0,0.75)"
+      card-color "#F0FFFF"
+      card-active-color "#80FFFF"
+      card-font-size "14pt"]
+  (defstyles screen
+    [:.fc-card
+     {:padding "1em",
+      :margin ".3em",
+      :border-radius ".75em",
+      :display "inline-block",
+      :min-width "6em",
+      :font-size card-font-size,
+      :background-color card-color,
+      :transition-duration "0.3s",
+      :box-shadow box-shadow}]
+    [:.fc-card:active
+     {:background-color card-active-color
+      :box-shadow (str "inset box-shadow")}]
+    [:.fc-card:focus
+     {:background-color card-color
+      :outline "none"}]
+    [:.fc-card:hover
+     {:background-color card-active-color}]
+
+    [:.subject-word
+     {:font-size "28pt"}]
+    )  )
+
+
