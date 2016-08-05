7a36509ecf167ccc17c4a0aa0cce19b9cfa27f92
David Goldfarb <deg@degel.com>
2016-08-05 12:10:53 +0300

    [For BugzId:464] Hebrew and Arabic recognition utilities

--- test/cljs/flashcards/runner.cljs
+++ test/cljs/flashcards/runner.cljs
@@ -1,7 +1,10 @@
 (ns flashcards.runner
     (:require [doo.runner :refer-macros [doo-tests]]
               [flashcards.core-test]
-              [flashcards.db-test]))
+              [flashcards.db-test]
+              [flashcards.handlers-test]
+              ))
 
 (doo-tests 'flashcards.core-test
-           'flashcards.db-test)
+           'flashcards.db-test
+           'flashcards.handlers-test)
